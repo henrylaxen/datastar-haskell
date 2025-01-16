d3edd5fa2ef88fad5d1fecb61060de638a2087aa
Henry Laxen <nadine.and.henry@pobox.com>
2025-01-16 11:49:59 -0700

    added $setup for doctest

---
+++ src/ServerSentEventGenerator.hs
@@ -0,0 +1,181 @@
+module ServerSentEventGenerator (
+
+    EventType(..)
+  , HttpVersion(..)
+  , ToBuilder(..)
+  , MergeMode(..)
+  , Send(..)
+  , MergeFragment
+  , sseHeaders
+  , send
+  , sendFragments
+  , mergeFragments
+  
+  ) where
+
+import           Constants
+import           Data.ByteString.Builder
+import           Data.Default              ( Default(..) )
+import ServerSentEventGenerator.Internal
+
+-- $setup
+-- >>> import           Data.Functor.Identity
+-- >>> import           Data.Maybe
+-- >>> import           Data.Text                 ( Text )
+-- >>> import qualified Data.Text.Encoding        as T
+
+
+-- import qualified Data.ByteString.Lazy      as B
+-- import           Data.ByteString.Lazy.UTF8
+-- import qualified Data.Text                 as T
+
+-- | returns the Http header for an SSE depending
+--   on the Http version you are using. Note: you will
+--   have to implement an instance of the HttpVersion class
+--   for whichever web server you are using
+--
+-- Example:
+--
+-- >>> builderToString . runIdentity $ sseHeaders
+-- "Cache-control: no-cache\nContent-type: text/event-stream\nConnection: keep-alive\n"
+
+sseHeaders :: HttpVersion m => m Builder
+sseHeaders = do
+  b <- isHttpVersion1_1
+  return $ if b then sseHeaders1_1 else sseHeaders2
+  where
+    sseHeaders2 = "Cache-control: no-cache\nContent-type: text/event-stream\n"
+    sseHeaders1_1 = sseHeaders2 <> "Connection: keep-alive\n"
+
+-- | A sum of the possible Datastar specific sse events that can be sent
+
+data EventType =
+    EventMergeFragments
+  | EventRemoveFragments
+  | EventMergeSignals
+  | EventRemoveSignals
+  | EventExecuteScript
+  deriving (Eq, Show)
+
+instance Default EventType
+  where def = EventMergeFragments
+
+instance ToBuilder EventType where
+  toBuilder EventMergeFragments   = cMergeFragments
+  toBuilder EventRemoveFragments  = cRemoveFragments
+  toBuilder EventMergeSignals     = cMergeSignals
+  toBuilder EventRemoveSignals    = cRemoveSignals
+  toBuilder EventExecuteScript    = cExecuteScript
+
+data MergeMode =
+     Morph
+   | Inner
+   | Outer
+   | Prepend
+   | Append
+   | Before
+   | After
+   | UpsertAttributes
+   deriving (Eq, Show)
+
+instance Default MergeMode
+  where def = Morph
+
+instance ToBuilder MergeMode where
+   toBuilder Morph            = cMorph
+   toBuilder Inner            = cInner
+   toBuilder Outer            = cOuter
+   toBuilder Prepend          = cPrepend
+   toBuilder Append           = cAppend
+   toBuilder Before           = cBefore
+   toBuilder After            = cAfter
+   toBuilder UpsertAttributes = cUpsertAttributes
+
+
+{- From the README.MD
+ServerSentEventGenerator.send(
+    eventType: EventType,
+    dataLines: string[],
+    options?: {
+        eventId?: string,
+        retryDuration?: durationInMilliseconds
+    }) -}
+
+data Send = Send {
+    sendEventType     :: EventType
+  , sendDataLines     :: [Builder]
+  , sendEventId       :: Maybe Builder
+  , sendRetryDuration :: Maybe Int
+  } deriving Show
+
+instance Default Send where
+  def = Send EventMergeFragments [] Nothing Nothing
+
+
+-- | convert a Send data type to a Builder, ready to be sent down the wire
+--
+-- Example
+--
+-- >>>  send $ Send EventMergeFragments ["line 1", "line 2"] (Just "alpah123") Nothing
+-- "datastar-merge-fragments\nid: alpah123\ndata: line 1\ndata: line 2\n\n"
+--
+-- >>> send (def {sendDataLines = ["line1, line2"]})
+-- "datastar-merge-fragments\ndata: line1, line2\n\n"
+
+send :: Send -> Builder
+send s = format builders <> "\n"
+  where
+    builders = 
+        [Just (toBuilder (sendEventType s))]
+        <> options (sendEventId s) (sendRetryDuration s)
+      <> ( map (Just . ("data: " <>)) (sendDataLines s))
+
+-- | A convenience function that takes a list of ByteString/String/Text and
+--   outputs a Builder, assuming the rest of the Send Data Type fields are
+--   the defaults.
+--
+-- Example
+--
+-- >>> sendFragments (["l1", "l2"] :: [String])
+-- "datastar-merge-fragments\ndata: l1\ndata: l2\n\n"
+--
+-- >>> sendFragments (["l1", "l2"] :: [Text])
+-- "datastar-merge-fragments\ndata: l1\ndata: l2\n\n"
+
+sendFragments :: ToBuilder a => [a] -> Builder
+sendFragments s = send def {sendDataLines = map toBuilder s}
+
+data MergeFragment = MergeFragment {
+     mergeData              :: [Builder]
+   , mergeSelector          :: Maybe Builder    -- > selector: "abc123"
+   , mergeMode              :: Maybe MergeMode  -- > 
+   , mergesettleDuration    :: Maybe Int
+   , mergeUseUiewTransition :: Maybe Bool
+   , mergeEventId           :: Maybe Builder
+   , mergeRetryDuration     :: Maybe Int
+   } deriving Show
+
+instance Default MergeFragment where
+  def                    = MergeFragment {
+       mergeData              = []
+    ,  mergeSelector          = Nothing
+    ,  mergeMode              = Just Morph
+    ,  mergesettleDuration    = Just cDefaultSettleDurationMs
+    ,  mergeUseUiewTransition = Just False
+    ,  mergeEventId           = Nothing
+    ,  mergeRetryDuration     = Just cDefaultSseRetryDurationMs }
+
+mergeFragments :: MergeFragment -> Builder
+mergeFragments m = format builders <> "\n"
+  where
+    builders = 
+      [ Just (toBuilder EventMergeFragments) ]
+      <> options (mergeEventId m) (mergeRetryDuration m)
+      <> withDefaults
+      <> ( map (Just . ("data: " <>)) (mergeData m))
+    withDefaults :: [Maybe Builder]
+    withDefaults = 
+      [  maybeDefault  (mergeMode m)
+      ,  maybeDefault  (mergeUseUiewTransition m)
+      ,  maybeDefault  (mergeUseUiewTransition m) ]
+  
---
+++ src/ServerSentEventGenerator/Internal.hs
@@ -0,0 +1,72 @@
+module ServerSentEventGenerator.Internal where
+
+import           Constants
+import           Data.ByteString.Builder
+-- import qualified Data.ByteString.Lazy      as B
+import           Data.ByteString.Lazy.UTF8
+import           Data.Default              ( Default(..) )
+import           Data.Functor.Identity
+import           Data.Maybe
+-- import qualified Data.Text                 as T
+import           Data.Text                 ( Text )
+import qualified Data.Text.Encoding        as T
+
+class Monad m => HttpVersion m where
+  isHttpVersion1_1 :: m Bool
+
+instance HttpVersion Identity  where
+  isHttpVersion1_1 = return True
+
+class ToBuilder a where
+  toBuilder :: a -> Builder
+
+instance ToBuilder ByteString where
+  toBuilder = lazyByteString
+
+instance ToBuilder Text where
+  toBuilder = byteString . T.encodeUtf8
+
+instance ToBuilder String where
+  toBuilder = lazyByteString . fromString
+
+instance ToBuilder Bool where
+  toBuilder False = "false"
+  toBuilder True  = "true"
+
+-- | prints a Builder on STDOUT
+putBuilder :: Builder -> IO ()
+putBuilder = putStrLn . builderToString
+
+-- | convert a Builder to a String, mainly for debugging
+
+builderToString :: Builder -> String
+builderToString = toString . toLazyByteString
+
+-- | append linefeeds to list of Builders and add another one to the end
+
+withLineFeeds :: [Builder] -> Builder
+withLineFeeds = mconcat . map (<> "\n" )
+
+-- | All server sent events can contain and Event Id and a Retry Duration as an option
+
+options :: Maybe Builder -> Maybe Int -> [Maybe Builder]
+options mbEventId mbRetryDuration =
+  [
+    ("id: " <>)  <$> mbEventId,
+    if mbRetryDuration == Just cDefaultSseRetryDurationMs
+    then Nothing
+    else ((<>) "retry: " . intDec) <$> mbRetryDuration
+  ]
+
+-- | A convenience function which turns default values into Nothings
+
+maybeDefault
+  :: (Eq a, Default a, ToBuilder a) => Maybe a -> Maybe Builder
+maybeDefault Nothing =  Nothing
+maybeDefault (Just x) = if x == def then Nothing else (Just . toBuilder $ x)
+
+-- | Takes a list of Maybe Builder, throws away the Nothings, and appends line feeds
+--   to the rest, removing the Justs
+
+format :: [Maybe Builder] -> Builder
+format = withLineFeeds . catMaybes 
