5b56b91ce91219956358bb949d4e8bf444e8a551
Henry Laxen <nadine.and.henry@pobox.com>
2025-01-22 14:23:49 -0700

    working on doctests, fighting stage errors

--- src/ServerSentEventGenerator.hs
+++ src/ServerSentEventGenerator.hs
@@ -27,16 +27,20 @@ import Control.Exception
 import NeatInterpolation
 import Control.Concurrent.MVar
 import Control.Monad.IO.Class
-
+import           Data.Functor.Identity
 
 -- import ServerSentEventGenerator.Internal
 -- import ServerSentEventGenerator.Newtypes
 
 -- $setup
--- >>> import           Data.Functor.Identity
--- >>> import           Data.Maybe
--- >>> import           Data.Text                 ( Text )
--- >>> import qualified Data.Text.Encoding        as T
+{- | >>> :{
+import           Data.Functor.Identity
+import           Data.Maybe
+import           Data.Text                 ( Text )
+import qualified Data.Text.Encoding        as T
+:}
+-}
+
 
 -- <<Bug>> in sse.py, the event_id is an Int
 
@@ -69,36 +73,84 @@ send a b c = sendM (sendPure a b c)
 --   be removed from the output
 
 sendPure :: (ToBuilder a) => EventType -> [a] -> Options -> Builder
-sendPure eventType dataLines options = (buildLines (a:b:c)) <> "\n"
+sendPure eventType dataLines options = (buildLines (a:b:c)) <> "\n\n"
   where
-    withSSEdefault value defaultValue field = if value ==  defaultValue then mempty
+    withSSEdefault value defaultValue field = if value == defaultValue then mempty
       else field <> ": " <> toBuilder value
     a = "event: " <> toBuilder eventType
     b = toBuilder options
     c = map (\x -> cData <> ": " <> toBuilder x) dataLines
 
+{- | >>> :{
+do
+  sendM (mergeFragments sampleDataLines noSelector def def def)
+  sendM (mergeFragments sampleDataLines (SEL "#id") def def def)
+  sendM (mergeFragments sampleDataLines (SEL "#id") Inner def def)
+  sendM (mergeFragments sampleDataLines (SEL "#id") Inner (FO 1 False) def)
+  sendM (mergeFragments sampleDataLines (SEL "#id") Inner (FO 1 True) (O "abc123" 10))
+:}
+event: datastar-merge-fragments
+data: data: fragments line 1
+data: fragments line 2
+<BLANKLINE>
+event: datastar-merge-fragments
+data: data: selector #id
+data: fragments line 1
+data: fragments line 2
+<BLANKLINE>
+event: datastar-merge-fragments
+data: data: selector #id
+data: mergeMode inner
+data: fragments line 1
+data: fragments line 2
+<BLANKLINE>
+event: datastar-merge-fragments
+data: data: selector #id
+data: mergeMode inner
+data: settleDuration 1
+data: fragments line 1
+data: fragments line 2
+<BLANKLINE>
+event: datastar-merge-fragments
+id: abc123
+retry: 10
+data: data: selector #id
+data: mergeMode inner
+data: settleDuration 1
+data: useViewTransition true
+data: fragments line 1
+data: fragments line 2
+<BLANKLINE>
+-}
+
+{- |  :{
+test :: [Builder] -> IO ()
+test = mapM_ sendM
+ts :: [Builder]
+ts =
+  let
+    mt1,mt2,mt3,mt4,mt5 :: Builder
+    mt1 = mergeFragments sampleDataLines noSelector def def def
+    mt2 = mergeFragments sampleDataLines (SEL "#id") def def def
+    mt3 = mergeFragments sampleDataLines (SEL "#id") Inner def def
+    mt4 = mergeFragments sampleDataLines (SEL "#id") Inner (FO 1 False) def
+    mt5 = mergeFragments sampleDataLines (SEL "#id") Inner (FO 1 True) (O "abc123" 10)
+  in [mt1,mt2,mt3,mt4,mt5]
+ts
+:}
+abc
+-}
+
+
 mergeFragments :: (ToBuilder a) => [a] -> Selector a -> MergeMode -> FragmentOptions -> Options -> Builder
-mergeFragments fragments selector mode fragOptions options = buildLines (a:b:c:d:e:f)
+mergeFragments fragments selector mode fragOptions =  sendPure MergeFragments [buildLines (a:b:c:d)]
   where
-    a = "event: " <> toBuilder MergeFragments
-    b = toBuilder options
-    c = toBuilder selector
-    d = withDefault cMerge cDefaultMergeMode (toBuilder mode)
-    e = toBuilder fragOptions
-    f = withList cFragments fragments
+    a = toBuilder selector
+    b = withDefault cMerge cDefaultMergeMode (toBuilder mode)
+    c = toBuilder fragOptions
+    d = withList cFragments fragments
 
-xmergeFragments :: (ToBuilder a) => [a] -> Selector a -> MergeMode -> FragmentOptions -> Options -> Builder
-xmergeFragments fragments selector mode fragOptions =
-  sendPure MergeFragments (c:d:e:f)
-  where
-    a = "event: " <> toBuilder MergeFragments
---    b = toBuilder options
-    c = toBuilder selector
-    d = withDefault cMerge cDefaultMergeMode (toBuilder mode)
-    e = toBuilder fragOptions
-    f = withList cFragments fragments
 
--- map (\x -> "mt" <> show x) [1..6]
 mt1,mt2,mt3,mt4,mt5 :: Builder
 mt :: IO ()
 mt1 = mergeFragments sampleDataLines noSelector def def def
@@ -107,55 +159,25 @@ mt3 = mergeFragments sampleDataLines (SEL "#id") Inner def def
 mt4 = mergeFragments sampleDataLines (SEL "#id") Inner (FO 1 False) def
 mt5 = mergeFragments sampleDataLines (SEL "#id") Inner (FO 1 True) (O "abc123" 10)
 mt = test [mt1,mt2,mt3,mt4,mt5]
--- ----
--- event: datastar-merge-fragments
--- data: selector #id
--- data: fragments line 1
--- data: fragments line 2
--- ----
--- event: datastar-merge-fragments
--- data: selector #id
--- data: mergeMode inner
--- data: fragments line 1
--- data: fragments line 2
--- ----
--- event: datastar-merge-fragments
--- data: selector #id
--- data: mergeMode inner
--- data: settleDuration 1
--- data: fragments line 1
--- data: fragments line 2
--- ----
--- event: datastar-merge-fragments
--- id: abc123
--- retry: 10
--- data: selector #id
--- data: mergeMode inner
--- data: settleDuration 1
--- data: useViewTransition true
--- data: fragments line 1
--- data: fragments line 2λ> 
 
--- <<Bug>> in sse.py, the selector is made optional
-
+-- <<Bug>> in sse.py, the selector is made optional  
 removeFragments :: (ToBuilder a) => Selector a -> FragmentOptions -> Options -> Builder
-removeFragments selector fragOptions options = buildLines [a,b,c,d]
+removeFragments selector fragOptions = sendPure RemoveFragments [buildLines [a,b]]
   where
     s = toBuilder selector
-    a = "event: " <> toBuilder RemoveFragments
-    b = toBuilder options
-    c = if s == def then bug RemoveFragmentSelectorIsMissing else s
-    d = toBuilder fragOptions
+    a = if s == def then bug RemoveFragmentSelectorIsMissing else s
+    b = toBuilder fragOptions
 
+rt2,rt3,rt4,rt5 :: Builder
+rt, rt1 :: IO ()
 rt1 = test [removeFragments noSelector def def] `catch`
         (\(e :: ServerSentEventGeneratorExceptions) -> print e)
 rt2 = removeFragments (SEL ("#id" :: Builder)) def def
 rt3 = removeFragments (SEL ("#id" :: Text)) (FO 1 False) def
 rt4 = removeFragments (SEL ("#id" :: String)) (FO 1 True) def
 rt5 = removeFragments (SEL ("#id" :: ByteString)) (FO 1 False) (O "abc123" 10)
-rt6 = rt1 >> test [rt2,rt3,rt4,rt5]
+rt = rt1 >> test [rt2,rt3,rt4,rt5]
 
--- withDefault dStarEvent defaultValue value
 
 -- <<Bug>> in sse.py or README.md,
 -- sse.py has signals as an array, README.md has signals as a string
@@ -164,57 +186,58 @@ rt6 = rt1 >> test [rt2,rt3,rt4,rt5]
 -- convenient for programmers.  Of course it's up to you.
 -- if array -> mergeSignals :: (ToBuilder a) => [a] -> Bool -> Options -> Builder
 mergeSignals :: (ToBuilder a) => a -> Bool -> Options -> Builder
-mergeSignals signals onlyIfMissing  options = buildLines [a,b,c,d]
+mergeSignals signals onlyIfMissing = sendPure MergeSignals [buildLines [a,b]]
   where
-    a = "event: " <> toBuilder MergeSignals
-    b = toBuilder options
-    c = if (toBuilder signals) == mempty
+    a = if (toBuilder signals) == mempty
           then bug SignalsSelectorIsMissing
           else withDefault cSignals nil (toBuilder signals)
 -- if array -> else withList cSignals signals
-    d = withDefault cOnlyIfMissing cDefaultOnlyIfMissing onlyIfMissing
+    b = withDefault cOnlyIfMissing cDefaultOnlyIfMissing onlyIfMissing
 
+testMergeSignal :: Text
 testMergeSignal = [trimming|{"a":"b","c":true,"d":1}|]
+mst2,mst3 :: Builder
+mst, mst1 :: IO ()
 mst1 = test [mergeSignals nil def def] `catch`
         (\(e :: ServerSentEventGeneratorExceptions) -> print e)
 mst2 = mergeSignals  testMergeSignal False def
 mst3 = mergeSignals  testMergeSignal True (O "abc123" 10)
-mst4 = mst1 >> test [mst2,mst3]
+mst = mst1 >> test [mst2,mst3]
 
 -- <<bug>> Maybe? sse.py allows the paths to be empty,
 --                README.md does not specify
 removeSignals :: (ToBuilder a) => [a] -> Options -> Builder
-removeSignals paths options = buildLines (a:b:c)
+removeSignals paths = sendPure RemoveSignals [buildLines c]
   where
-    a = "event: " <> toBuilder RemoveSignals
-    b = toBuilder options
     c = withList cRemoveSignals paths
 testRemoveSignal = ["velocity.x", "velocity.y", "position"] :: [Builder]
-rst1 = removeSignals ([] :: [Builder]) def
+
+rst1,rst2,rst3 :: Builder
+rst :: IO ()
+rst1 = removeSignals (mempty :: [Builder]) def
 rst2 = removeSignals  testRemoveSignal def
 rst3 = removeSignals  testRemoveSignal (O "abc123" 10)
-rst4 :: IO ()
-rst4 = test [rst1,rst2,rst3]
+rst = test [rst1,rst2,rst3]
 
 -- <<bug>> Maybe? sse.py allows the script to be empty, and type is array
 --                README.md does not specify, and type is string
 executeScript :: (ToBuilder a, Eq b, Monoid b, ToBuilder b) =>
                  [a] -> [b] -> Bool -> Options -> Builder
-executeScript script attributes autoRemove options = buildLines ((a:b:c) <> d <> [e])
+executeScript script attributes autoRemove = sendPure ExecuteScript [buildLines (a <> b <> [c])  ]
   where
-    a = "event: " <> toBuilder ExecuteScript
-    b = toBuilder options
-    c = withList cExecuteScript script
-    d = if null attributes
+    a = withList cExecuteScript script
+    b = if null attributes
           then [cData <> ": " <> cAttributes <> " " <> cDefaultAttributes]
           else withList cAttributes attributes
-    e = withDefault cAutoRemove cDefaultAutoRemove autoRemove
+    c = withDefault cAutoRemove cDefaultAutoRemove autoRemove
 testScript     = [[trimming|window.location = "https://data-star.dev"|]]
 testAttributes = [[trimming|type text/javascript|]]
 noList = [] :: [Builder]
+
+est1,est2,est3,est4 :: Builder
+est :: IO ()
 est1 = executeScript noList noList True def
 est2 = executeScript  testScript noList False def
 est3 = executeScript  testScript testAttributes False def
 est4 = executeScript  testScript testAttributes True (O "abc123" 10)
-est5 :: IO ()
-est5 = test [est1,est2,est3,est4]
+est  = test [est1,est2,est3,est4]
--- src/ServerSentEventGenerator/Internal.hs
+++ src/ServerSentEventGenerator/Internal.hs
@@ -5,6 +5,48 @@ import ServerSentEventGenerator.Constants
 import Data.ByteString.Builder
 import Control.Monad.IO.Class
 import Control.Concurrent
+import System.IO
+
+-- | Combines a list of Builders into a single Builder, using the same mechanism
+--   as the more commonly known functions wunWrds or unLines.  A line feed is
+--   inserted between each builder in the list.  Empty builders are removed, so
+--   there are no blank lines.
+
+-- | >>> putStr "Hello\nWorld!"
+-- Hello
+-- World!
+
+
+{- |  :{
+sp = hPutBuilder stdout
+wa = "a" :: Builder
+wb = "b" :: Builder
+wprefix = "prefix" :: Builder
+nil = "" :: Builder
+enclose :: Builder -> Builder
+enclose x = "[" <> x <> "]\n"
+w1 = withDefault wprefix wa wa
+w2 = withDefault wprefix wa wa
+w3 = withDefault wprefix wb wa
+w4 = withDefault wprefix wa wb
+w5 = withDefault wprefix nil nil
+w6 = withDefault wprefix wa nil
+w7 = withDefault wprefix nil wa
+w8 = [enclose w1, enclose w2, enclose w3, enclose w4, enclose w5, enclose w6, enclose w7]
+sp w8
+:}
+[]
+[]
+[data: wprefix wa]
+[data: wprefix wb]
+[]
+[]
+[data: wprefix wa]
+-}
+
+{- |  :{
+:}
+-}
 
 buildLines :: [Builder] -> Builder
 buildLines builders = (go mempty builders)
@@ -18,14 +60,15 @@ buildLines builders = (go mempty builders)
     go acc (b:bs) = if b == mempty then go acc bs else b <> "\n" <> go acc bs
 
 withDefault :: (Eq a, ToBuilder a, ToBuilder b) => b -> a -> a -> Builder
-withDefault dStarEvent defaultValue value = if value == defaultValue
+withDefault dStarEvent defaultValue value =
+  if value == defaultValue || toBuilder value == mempty
   then mempty
   else cData <> ": " <> toBuilder dStarEvent <> " " <>  toBuilder value
 
 withList :: (ToBuilder a) => Builder -> [a] -> [Builder]
 withList name =  map (\x -> cData <> ": " <> name <> " " <> toBuilder x)
 
-  -- | Send a list of Builders, as a unit (single treaded) to the server dependent
+-- | Send a list of Builders, as a unit (single treaded) to the server dependent
 --   sse function, which is the sole method of the SSE class
 
 sendM :: MonadIO m => Builder -> m ()
@@ -40,3 +83,40 @@ sendM b = liftIO $ do
 
 test :: MonadIO m => [Builder] -> m ()
 test = mapM_ sendM
+
+rr :: String -> Int -> IO ()
+rr x n = do
+  let a = concatMap (\y -> ("," <> x <> show y) ) [1 .. n]
+  putStr . Prelude.drop 1 $ a
+  putStrLn " :: Builder"
+  putStrLn (x <> " :: IO ()")
+
+
+
+-- wa = "a" :: Builder
+-- wb = "b" :: Builder
+-- wprefix = "prefix" :: Builder
+-- nil = "" :: Builder
+-- enclose :: Builder -> Builder
+-- enclose x = "[" <> x <> "]\n"
+-- w1 = withDefault wprefix wa wa
+-- w2 = withDefault wprefix wa wa
+-- w3 = withDefault wprefix wb wa
+-- w4 = withDefault wprefix wa wb
+-- w5 = withDefault wprefix nil nil
+-- w6 = withDefault wprefix wa nil
+-- w7 = withDefault wprefix nil wa
+-- w8 = [enclose w1, enclose w2, enclose w3, enclose w4, enclose w5, enclose w6, enclose w7]
+-- w :: IO ()
+-- w = mapM_ sp w8
+
+
+
+-- []
+-- []
+-- [data: wprefix wa]
+-- [data: wprefix wb]
+-- []
+-- []
+-- [data: wprefix wa]
+
